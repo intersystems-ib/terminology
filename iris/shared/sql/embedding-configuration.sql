48554c245606d8a416d549036b5e15fc87757334
isc-lperezra <lperezra@intersystems.com>
2026-04-17 22:29:12 +0200

    TermEmbedding creation and Embedding config by SQL

---
+++ iris/shared/sql/embedding-configuration.sql
@@ -0,0 +1,27 @@
+USE DATABASE "TERMINOLOGY"
+GO
+INSERT INTO %Embedding.Config (Name, Configuration, EmbeddingClass, Description)
+  VALUES ('sentence-transformers-config',
+          '{"modelName":"FremyCompany/BioLORD-2023-M",
+            "hfCachePath":"/tmp/hfCache",
+            "maxTokens": 768,
+            "checkTokenCount": true}',
+          '%Embedding.SentenceTransformers',
+          'BioLord-2023-M model from Hugging Face, optimized for biomedical text.')
+GO
+CREATE TABLE IF NOT EXISTS Terminology_Vector.TermEmbedding (
+    TermId INT NOT NULL,
+    Terminology VARCHAR(100) NOT NULL,
+    Code VARCHAR(40) NOT NULL,
+    Lang VARCHAR(10),
+    Description VARCHAR(2000) NOT NULL,
+    DescriptionEmbedding EMBEDDING ('sentence-transformers-config', 'Description')
+)
+GO
+CREATE UNIQUE INDEX TermEmbedding_Unique ON TABLE Terminology_Vector.TermEmbedding (TermId, Terminology, Code, Lang)
+GO
+CREATE INDEX TermEmbedding_Code ON TABLE Terminology_Vector.TermEmbedding (TermId, Terminology)
+GO
+CREATE INDEX HNSWIndexTerm ON TABLE Terminology_Vector.TermEmbedding (DescriptionEmbedding)
+  AS HNSW(M=24, Distance='DotProduct')
+GO
